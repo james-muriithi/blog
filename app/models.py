ef0e5fd0aeebb63a34052f10bce285cf1ea6f8e1
james-muriithi <muriithijames556@gmail.com>
2022-02-12 17:24:27 +0300

    added models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from email.policy import default
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager,db
@@ -17,7 +18,7 @@ class User(UserMixin, db.Model):
     about = db.Column(db.Text)
     avatar = db.Column(db.String(64))
 
-    # pitches = db.relationship('Pitch', backref="user", lazy="dynamic")
+    blogs = db.relationship('Blog', backref="user", lazy="dynamic")
 
     created_at = db.Column(db.DateTime, index=True, default=datetime.now)
 
@@ -42,68 +43,61 @@ class User(UserMixin, db.Model):
         return User.query.get(int(user_id))
 
     def __repr__(self):
-        return f'User {self.username}'   
+        return f'User {self.first_name}'   
 
-
-class Pitch(db.Model):
+class Blog(db.Model):
     '''
-    Pitches table
+    Blogs table
     '''
-    __tablename__ = 'pitches'
-    
-    id = db.Column(db.Integer,primary_key = True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.Text)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    title = db.Column(db.String(255))
+    content = db.Column(db.String())
+    image_path = db.Column(db.String())
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    comments = db.relationship('Comment', backref='pitch', lazy="dynamic")  
-    votes = db.relationship('Vote', backref='pitch', lazy="dynamic")
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
-    created_at = db.Column(db.DateTime, index=True, default=datetime.now)
+    def __repr__(self):
+        return f'Post {self.title}'
+
 
 class Category(db.Model):
-    '''
-    Pitch categories table
-    '''
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    picture_path = db.Column(db.String(64))
-    pitch = db.relationship('Pitch', backref='category', lazy='dynamic')
-
-    # get all categories
-    @staticmethod
-    def get_all_categories():
-        return Category.query.all()
-
-    # save category
-    def save_category(self):
-        db.session.add(self)
-        db.session.commit()
+    name = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    blogs = db.relationship('Blog', backref='category', lazy='dynamic')
 
     def __repr__(self):
         return f'Category {self.name}'
-
-class Vote(db.Model):
-    '''
-    Votes table
-    '''
-    __tablename__ = 'votes'
-    id = db.Column(db.Integer, primary_key=True)
-    vote = db.Column(db.Integer)
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-
 
 class Comment(db.Model):
     '''comments table'''
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
     comment = db.Column(db.Text)
     created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)  
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls):
+        comments = Comment.query.all()
+        return comments
+
+     # delete comment
+    @classmethod
+    def delete_comment(cls, id):
+        comment = Comment.query.filter_by(id=id).first()
+        db.session.delete(comment)
+        db.session.commit()
 
 class Role(db.Model):
     '''
@@ -116,3 +110,22 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'Role {self.name}'
+
+class Subscriber(db.Model):
+    __tablename__ = 'subscribers'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    subscribed = db.Column(db.Integer, default=1)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_subscribers(cls):
+        subscribers = Subscriber.query.filter_by(subscribed =1).all()
+        return subscribers
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
